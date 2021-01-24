a6036667e340f94e33e4f59f866c6e3a89100f26
absoluteRango <31278752+absoluteRango@users.noreply.github.com>
2021-01-24 13:19:28 +0100

    Removed dist

--- src/app/index.js
+++ src/app/index.js
@@ -2,13 +2,13 @@ const { sortable } =  require('./functions');
 const { Component } = require('./components/base.component'); 
 
 const render = app => {
-    const listItems = [];
-    for (let i = 0; i < 5; ++i) {
-        const listItem = new Component({ className: 'list--item', text: { title: `derp ${[i+1]}`, type: 'p' } }).render();
-        listItems.push(listItem)
-    }
-    const list = new Component({ children: listItems, className: 'list'}).render();
     for (let i = 0; i < 3; ++i) {
+        const listItems = [];
+        for (let j = 0; j < 10; ++j) {
+            const listItem = new Component({ className: 'list--item', text: { title: `${[i+1]}.${j}`, type: 'p' } }).render();
+            listItems.push(listItem)
+        }
+        const list = new Component({ children: listItems, className: 'list'}).render();
         const container = new Component({ children: list, className: 'container', text: { title: `List ${[i+1]}`, type: 'h3' } }).render();
         app.insertAdjacentHTML('beforeend', container);
     }
